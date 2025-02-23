421c10257d99d72f4799a31a2c2f387e12be9abd
aaubertin <paubertin8@gmail.com>
2025-02-23 15:25:31 +0100

    ok

---
+++ cpp00/PhoneBook.hpp
@@ -0,0 +1,15 @@
+#include "Contact.hpp"
+
+class PhoneBook {
+private:
+    Contact contacts[8];
+    int currentIndex;
+    int totalContacts;
+
+public:
+    PhoneBook();
+    void addContact(const Contact& contact);
+    void searchContact() const;
+    void displayContactList() const;
+    void displayContact(int index) const;
+};
